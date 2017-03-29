4eef05ddddb22b6f6e38734473a7d779c076d793
guilherme kenzo <YOUR EMAIL ADDRESS>
2017-03-29 14:08:33 -0300

    Fixed removeStopWords() and added nGrammer()

--- main.py
+++ main.py
@@ -13,67 +13,74 @@ class PdfText(object):
         self.text = ""
         self.sentTokens = []
         self.sentTokensNoStopWords = []
-        self.fileName = ""
+        self.fileName = pdfFile
         self.citations = []
         self.pageList = []
-        self.getText(pdfFile)
+        self.getText()
         self.splitToList()
         self.nPages = ""
         self.ngrams = []
-        self.tokenizer()
+        self.removeMatching()
 
 
-    def getText(self, pdfFile):
+    def removeMatching(self):
+        self.text = re.sub('página \d ?\d? de \d\d',' ' , self.text)
+        self.text = re.sub('documento: \d+ - inteiro teor do acórdão - site certificado - dje: \d{2}/\d{2}/\d{4}',' ' ,self.text)
+
+    def getText(self):
         """ extract text from pdf file"""
         try:
-            self.text = textract.process(pdfFile).decode('utf-8').lower()
+            self.text = textract.process(self.fileName).decode('utf-8').lower().replace('\n', ' ')
             return self.text
         except textract.exceptions.MissingFileError as e:
             raise e
 
 
-    def removeStopWords(self, sentences):
+    def removeStopWords(self):
         """Remove stopWords from a given list of sentences"""
         stopWords = stopwords.words('portuguese')
+        noPunctSents = [self.stripPunct(i) for i in self.sentTokens]
+        # sentences = self.stripPunct(self.sentTokens)
+        # print(sentences)
         newList = []
-        for i in sentences:
-            for j in stopWords:
-                i = i.replace(j, '')
-            newList.append(i)
-        return newList
+        for sent in noPunctSents:
+            tmpSent = [i for i in sent.split() if i not in stopWords]
+            newList.append(' '.join(tmpSent))
 
+        self.sentTokensNoStopWords = newList
+        return self.sentTokensNoStopWords
+
+    def stripPunct(self, textString):
+        """remove punctuation"""
+        table = str.maketrans({key: None for key in punctuation})
+        return textString.translate(table)
+        # newList = []
+        # for i in tokens:
+        #     newList.append(i.translate(table))
+        # return newList
 
     def tokenizer(self):
         """Tokenize string to list of sentences"""
         sentTokenizer = load('tokenizers/punkt/portuguese.pickle')
         self.sentTokens = sentTokenizer.tokenize(self.text)
-        self.sentTokensNoStopWords = removeStopWords(self.sentTokens)
-        self.sentTokensNoStopWords = cleanUp(self.sentTokensNoStopWords)
+        # self.sentTokensNoStopWords = removeStopWords(self.sentTokens)
+        # self.sentTokensNoStopWords = stripPunct(self.sentTokensNoStopWords)
         return self.sentTokens
-
-
-    def cleanUp(self, stoplessTokens):
-        """remove punctuation"""
-        table = str.maketrans({key: None for key in punctuation})
-        newList = []
-        for i in stoplessTokens:
-            newList.appen(i.translate(table))
-        return newList
 
 
     def splitToList(self):
         """Splits document into list of pages, with each item corresponding
         to a page in the pdf file"""
-        self.pageList = re.split('Página +\d+ +de +\d+', self.text)
+        self.pageList = re.split('página \d ?\d? de \d\d', self.text)
         self.nPages = len(self.pageList)
         return self.pageList
 
 
-    def find_ngrams(self, tokenList, n):
+    def nGrammer(self, tokenList, n):
         """Returns N-gram of list of sentences"""
         newList = []
         for i in tokenList:
-            newList.append(zip(*[input_list[i:] for i in range(n)]))
+            newList.append(list(zip(*[tokenList[i:] for i in range(n)])))
         self.ngrams = newList
         return self.ngrams
 
@@ -88,3 +95,7 @@ class PdfText(object):
 
 if __name__ == '__main__':
     obj = PdfText('ITA.pdf')
+    obj.tokenizer()
+    obj.removeStopWords()
+    obj.sentTokensNoStopWords
+    
